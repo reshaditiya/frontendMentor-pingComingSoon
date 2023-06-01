b52ee5d4c7cbf19020655d06853190aaae7e1eb2
reshaditiya <reshaditiya@gmail.com>
2023-06-01 23:59:19 +0700

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+// UNOCSS
+window.__unocss = {
+	theme: {
+		colors: {
+			primaryBlue: "hsl(223, 87%, 63%)",
+			secondaryBlue: "hsl(223, 100%, 88%)",
+			secondaryRed: "hsl(354, 100%, 66%)",
+			neutralGray: "hsl(0, 0%, 59%)",
+			neutralBlue: "hsl(209, 33%, 12%)",
+		},
+		fontFamily: {
+			"libre-franklin": "'Libre Franklin', sans-serif",
+		},
+	},
+	shortcuts: {},
+}
